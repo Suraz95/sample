7a733e1d0c1bc9a4d2eefeb11cf74b1bfbcebfd4
Suraz95 <shaik.suraz50@gmail.com>
2024-07-22 16:51:47 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("index")
+import express from "express"
+// import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config({
+  path: "./.env",
+});
+const app=express()
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log("port is connected");
+});
+app.get("/", (req, res) => {
+    res.send("Home page")
+})
